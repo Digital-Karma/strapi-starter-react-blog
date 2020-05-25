014fe6163d5a7dea22d6ffec854655211164110f
jessy Muller <fenrir.117@outlook.fr>
2020-05-25 16:31:43 +0200

    modif title

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Query from "../Query";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Query from '../Query';
+import { Link } from 'react-router-dom';
 
-import CATEGORIES_QUERY from "../../queries/category/categories";
+import CATEGORIES_QUERY from '../../queries/category/categories';
 
 const Nav = () => {
   return (
@@ -11,23 +11,23 @@ const Nav = () => {
         {({ data: { categories } }) => {
           return (
             <div>
-              <nav className="uk-navbar-container" data-uk-navbar>
-                <div className="uk-navbar-left">
-                  <ul className="uk-navbar-nav">
+              <nav className='uk-navbar-container' data-uk-navbar>
+                <div className='uk-navbar-left'>
+                  <ul className='uk-navbar-nav'>
                     <li>
-                      <Link to="/">Strapi Blog</Link>
+                      <Link to='/'>Mon Blog</Link>
                     </li>
                   </ul>
                 </div>
 
-                <div className="uk-navbar-right">
-                  <ul className="uk-navbar-nav">
+                <div className='uk-navbar-right'>
+                  <ul className='uk-navbar-nav'>
                     {categories.map((category, i) => {
                       return (
                         <li key={category.id}>
                           <Link
                             to={`/category/${category.id}`}
-                            className="uk-link-reset"
+                            className='uk-link-reset'
                           >
                             {category.name}
                           </Link>
